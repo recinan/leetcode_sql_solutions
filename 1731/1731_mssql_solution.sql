20857f9764aaf5fecbafbae551f6809bfb472a47
Recep Inan <recinan.ceng@gmail.com>
2025-05-19 22:21:16 +0300

    1731. The Number of Employees Which Report to Each Employee

---
+++ 1731/1731_mssql_solution.sql
@@ -0,0 +1,7 @@
+/* 1731. The Number of Employees Which Report to Each Employee */
+SELECT E1.employee_id,  E1.name, COUNT(E2.employee_id) AS reports_count, ROUND(AVG(E2.age*1.00),0) AS average_age
+FROM Employees E1 JOIN Employees E2 
+ON E1.employee_id = E2.reports_to
+GROUP BY E1.employee_id,  E1.name
+ORDER BY E1.employee_id
+
